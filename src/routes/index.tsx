a36886e6c71984bf96cd5b19b857a8ae169b87f4
Itair Filho <itair1068@gmail.com>
2023-04-07 22:13:43 -0300

    Reestruturação de pastas e tema claro adicionado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Button } from '@mui/material'
+import {Routes, Route, Navigate} from 'react-router-dom'
+
+export function AppRoutes () {
+    return (
+        <Routes>
+            <Route path ='pagina-inicial' element={<Button variant='contained' color='primary'>Teste</Button>} />
+
+            <Route path ='*' element={<Navigate to='/pagina-inicial' />} />
+        </Routes>
+    )
+ };
