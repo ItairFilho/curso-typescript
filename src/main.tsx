472bedab93413497d097f07ad5d17ee3794f81d8
Itair Filho <itair1068@gmail.com>
2023-04-14 09:05:18 -0300

    Adicionado tema escuro e troca de tema

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter} from 'react-router-dom'
 import { AppRoutes } from './routes'
-import { ThemeProvider } from '@emotion/react'
-import { LightTheme } from './shared/themes'
+import { AppThemeProvider } from './shared/contexts/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={LightTheme}>
-          <BrowserRouter>
-                 <AppRoutes />
-          </BrowserRouter>
-    </ThemeProvider>      
-  </React.StrictMode>,
+  <AppThemeProvider>
+              <BrowserRouter>
+                      <AppRoutes />
+              </BrowserRouter>
+       </AppThemeProvider>   
+       </React.StrictMode>,
 )
